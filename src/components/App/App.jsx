48b558679e28919dd796c2275b75abb2d6ea1e45
SergeyKardashev <angeldeep@yandex.ru>
2023-12-06 17:11:19 +0700

    feat: сверстать шапку и добавить пакет PropTypes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import logo from '../../logo.svg';
 import './App.css';
+import Header from '../Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* отправляю в верстку 2 компонента */}
+
+      {/* первый - в темной теме. isDark без значения - по дефолту true */}
+      ======= logged OUT ======
+      <Header isDark isLoggedIn={false} />
+      ======= logged in ======
+      <Header isDark isLoggedIn />
+
+      {/* В светлой теме. isDark = false */}
+      ======= logged in ======
+      <Header isDark={false} isLoggedIn />
+    </>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.css';
+import logoPath from '../../images/logo.svg';
+import iconAvaRoundPath from '../../images/icon_ava_round.svg';
+
+function Header(props) {
+  const { isDark, isLoggedIn } = props;
+  const headerClassName = `header ${isDark && 'header_dark'}`;
+  const headerNavClassName = `header__nav ${!isLoggedIn && 'header__nav_hidden'}`;
+  const headerNavLinkClassName = `header__nav-link ${isDark && 'header__nav-link_dark'}`;
+
+  const headerAccountBtnClassName = `header__account-btn
+  ${isDark && 'header__account-btn_dark'}
+  ${!isLoggedIn && 'header__account-btn_hidden'}`;
+
+  const headerMenuClassName = `header__menu
+  ${isDark && 'header__menu_dark'}
+  ${!isLoggedIn && 'header__menu_hidden'}`;
+
+  const headerAuthClassName = `header__auth
+  ${isLoggedIn && 'header__auth_hidden'}
+  ${!isDark && 'header__auth_hidden'}`;
+
+  return (
+    <header className={headerClassName}>
+      <img className="logo" src={logoPath} alt="Лого" />
+      <nav className={headerNavClassName}>
+        <ul className="header__nav-list">
+          <li className="header__nav-item">
+            <a className={headerNavLinkClassName} href="https://ya.ru/">
+              Фильмы
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <a className={headerNavLinkClassName} href="https://ya.ru/">
+              Сохранённые фильмы
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className={headerAccountBtnClassName}>
+        <div className="header__account-txt">
+          Аккаунт
+        </div>
+        <img className="header__account-icon" src={iconAvaRoundPath} alt="иконка юзера" />
+      </div>
+      <button className={headerMenuClassName} type="button" aria-label="меню" />
+      <div className={headerAuthClassName}>
+        <div className="header__signup">Регистрация</div>
+        <button className="header__signin" type="button">Войти</button>
+      </div>
+    </header>
+  );
+}
+Header.propTypes = {
+  isDark: PropTypes.bool.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
+};
+
+export default Header;
